daf723bfc266e2a9b18762714335b6b959c6c1f9
Ohara <nori.tukareta@gmail.com>
2020-05-10 23:06:32 +0900

    アニメーション仮組、スワイプのclass作成

---
+++ N'sSimpleGame/N'sSimpleGame/Model/Swipe.swift
@@ -0,0 +1,14 @@
+//
+//  Swipe.swift
+//  N'sSimpleGame
+//
+//  Created by 尾原徳泰 on 2020/05/10.
+//  Copyright © 2020 尾原徳泰. All rights reserved.
+//
+
+import Foundation
+
+class SwipeAction {
+    //スワイプアクション用のスワイプリコグナイザーを作る
+    
+}

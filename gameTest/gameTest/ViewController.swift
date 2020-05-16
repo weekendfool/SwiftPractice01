8c7c4776b00ef1bee1e8f1a22a2c109dcbc455fc
Ohara <nori.tukareta@gmail.com>
2020-05-16 23:56:39 +0900

    問題の表示実装

--- gameTest/gameTest/ViewController.swift
+++ gameTest/gameTest/ViewController.swift
@@ -11,23 +11,46 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var targetImageView: UIImageView!
-    @IBOutlet var problemLabel: UILabel!
+    @IBOutlet weak var problemLabel: UILabel!
+    
+    var flag = true
+    var num = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //初期化
+        problemLabel.text = ""
         //スワイプ検知の関数
         setSwipeAction()
+       
+ 
+        doGame()
+        
         
     }
     //問題のリスト
     var list:[String] = ["上", "右", "下", "左"]
     
     func random() {
-        var number = Int.random(in: 0...3)
-        var text = list[number]
+        let number = Int.random(in: 0...3)
+        let text = list[number]
+        print(text)
         problemLabel.text = text
     }
-    
+    //回数制限
+    func doGame() {
+        var num = 0
+        print("3##########3")
+        while num <= 10 {
+            print("1!!!!!!!!!!!!!!!!!!!")
+            if flag == true {
+                random()
+                num += 1
+            } else {
+                print("$$$$$$")
+            }
+        }
+    }
     //スワイプの方向の列挙体を作成
     enum swipeActionDirection: Int {
         case up = 0
@@ -84,24 +107,27 @@ class ViewController: UIViewController {
         case "up":
             //imageviewを作成してスワイプされた方向にアニメーションさせる
             UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseIn, animations: {
-                moveImageView.center.y -= 100
+                moveImageView.center.y -= 200
                 selectDirection = swipeActionDirection.up.rawValue
             }) { (completed) in
                 moveImageView.removeFromSuperview()
+                self.flag = true
             }
         case "right":
             UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseIn, animations: {
-            moveImageView.center.x += 100
+            moveImageView.center.x += 200
             selectDirection = swipeActionDirection.right.rawValue
             }) { (completed) in
             moveImageView.removeFromSuperview()
+            self.flag = true
             }
         case "down":
             UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseIn, animations: {
-            moveImageView.center.y += 100
+            moveImageView.center.y += 200
             selectDirection = swipeActionDirection.down.rawValue
             }) { (completed) in
             moveImageView.removeFromSuperview()
+            self.flag = true
             }
         case "left":
             UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseIn, animations: {
@@ -109,6 +135,7 @@ class ViewController: UIViewController {
             selectDirection = swipeActionDirection.left.rawValue
             }) { (completed) in
             moveImageView.removeFromSuperview()
+            self.flag = true
             }
         default:
             return 6

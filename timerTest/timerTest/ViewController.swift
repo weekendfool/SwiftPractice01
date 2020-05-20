751e5c9772f7ff2bf226a3318756c8a3f4165ca1
Ohara <nori.tukareta@gmail.com>
2020-05-20 23:40:40 +0900

    タイマ実験

--- timerTest/timerTest/ViewController.swift
+++ timerTest/timerTest/ViewController.swift
@@ -34,5 +34,43 @@ class ViewController: UIViewController {
         
     }
     
+    func startTimer() {
+            //タイマーを1秒おきに起動
+            //タイマーを起動するためだけに一度だけ起動
+            self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(CountTimer.doTimer), userInfo: nil, repeats: true)
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!")
+        }
+        //1秒毎の処理（設定時間を引数にする）
+        //よばれ続ける
+        @objc func doTimer(setTime:Int) -> Int {
+            //時間を1秒おきに加算
+            countTimer += 1
+            //残り時間の計算
+            let remainingTime = setTime - countTimer
+            //残り時間が0になったときの処理
+    //        stopTimer(remainingTime: remainingTime)
+            //残り時間を返す
+    //        let tringRemainingTime = String(remainingTime)
+            print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+            if remainingTime == 0 {
+            //タイマーを停止する
+            self.timer.invalidate()
+            }
+            return remainingTime
+        }
+        
+        func stopTimer(remainingTime:Int) {
+            print("######################3")
+            //残り時間が0になったときの処理
+            if remainingTime == 0 {
+                //タイマーを停止する
+                self.timer.invalidate()
+      
+            }
+            
+        }
+    }
+
+    
 }
 
